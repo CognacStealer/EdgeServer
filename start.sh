e76141d6a4fb8f76ccd84cfceeb311be1f236539
Aman Aggarwal <142648283+CognacStealer@users.noreply.github.com>
2025-10-22 12:53:04 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Activate virtual environment
-source "$(dirname "$0")/.venv/bin/activate"
-
-# Go to project root
-cd "$(dirname "$0")"
-
-# Run FastAPI server
-python -m uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
